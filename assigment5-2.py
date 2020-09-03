d9556fe6339a2ce6e74a6309bff33569e2887429
thorsteinn <thorsteinn17@ru.is>
2020-09-03 14:44:33 +0000

    secont coment

--- assigment5-2.py
+++ assigment5-2.py
@@ -5,7 +5,10 @@ number_1 = 1
 number_2 = 2
 number_3 = 3 
 next_number = 0
-
+n=n-2
+print(number_1)
+print(number_2)
+print(number_3)
 for i in range(1,n):
     
     next_number = number_1+number_2+number_3
